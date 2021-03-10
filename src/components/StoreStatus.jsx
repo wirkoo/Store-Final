7ea91398b1bd1954ba575de60772e47cab904965
Waleed Technologist <waleed.technologist@gmail.com>
2021-03-10 15:02:23 +0500

    changing the directory name

--- src/components/StoreStatus.jsx
+++ src/components/StoreStatus.jsx
@@ -38,6 +38,7 @@ class StoreStatus extends Component {
     e.preventDefault();
     console.log(this.state.account);
     //Call the server
+
     console.log("submitted");
   };
   handleChange = (e) => {
